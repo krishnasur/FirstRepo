31af60d612d230f49368b5b6e645679f99b5f0c1
krishnasur <krishnasurendran20@gmail.com>
2021-06-27 17:30:02 +0530

    initial commit

---
+++ WEBAPIMVCProject/WEBAPI/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WEBAPI.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        
+        [Route("api/Customers/GetCustomers")]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            using (KrishnaDBEntities2 krishnaDBEntities = new KrishnaDBEntities2())
+            {
+                //var clist= krishnaDBEntities.Customers.ToList();
+                PostCustomers();
+                return krishnaDBEntities.Customers.ToList();
+            }
+        }
+        public void PostCustomers()
+        {
+            KrishnaDBEntities2 krishnaDBEntities = new KrishnaDBEntities2();
+            //insert data into another table
+            CustomersWrite customersWrite = new CustomersWrite();
+            //customersWrite.CustomerID = 1;
+            customersWrite.CustomerName = "test";
+            customersWrite.ContactName = "test";
+            customersWrite.City = "test";
+            customersWrite.Address = "test";
+            customersWrite.Country = "test";
+            krishnaDBEntities.CustomersWrites.Add(customersWrite);
+            krishnaDBEntities.SaveChanges();
+        }
+
+    }
+}
---
+++ WEBAPIMVCProject/WEBAPI/Models/Customers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBAPI.Models
+{
+    public class Customers
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string ContactName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+
+    }
+}
